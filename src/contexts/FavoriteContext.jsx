6cf8756862870a38f0f4894f5ac01257188b8034
JHGit-hub <julien.hardy2104@gmail.com>
2025-08-15 09:30:16 +0200

    memo sur prochaine etape

---
+++ src/contexts/FavoriteContext.jsx
@@ -0,0 +1,15 @@
+/* 
+
+faire le context favorite pour créer un tableau des favrois selon leur id
+
+extraire et affiche la liste des favoris
+
+créer peti logo bookmark sur les recettes favorite selon condition "fais parti des favoris"
+
+mettre compteur favorite.length sur le logo favorite de la navbar
+
+
+
+
+
+*/
