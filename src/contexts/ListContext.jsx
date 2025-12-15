86c901c1e1845948039212dd06585bf8a2404a87
JHGit-hub <julien.hardy2104@gmail.com>
2025-12-15 13:14:17 +0100

    Ajustement fichier json pour vercel

--- src/contexts/ListContext.jsx
+++ src/contexts/ListContext.jsx
@@ -5,15 +5,24 @@ const ListContext = createContext();
 
 export default function ListProvider({ children }) {
 
-    const [ list, setList ] = useState([]);
+    const [list, setList] = useState([]);
 
     // On charge la liste des recettes depuis le fichier JSON lors du montage du composant
+    useEffect(() => {
+        fetch('/data/recettes_cuisine.json')
+            .then(response => response.json())
+            .then(data => setList(data))
+            .catch(error => console.error('Erreur lors du chargement du fichier JSON', error));
+    }, []);
+
+
+    {/*
     useEffect(() => {
         fetch('src/assets/recettes_cuisine.json')
             .then(response => response.json())
             .then(data => setList(data)) // On attribue la liste des recettes à la variable list
             .catch(error => console.error('Erreur lors du chargement du fichier JSON', error)); // On affiche un message d'erreur si une erreur se produit
-    }, []);
+    }, []); */}
 
 
     return (
